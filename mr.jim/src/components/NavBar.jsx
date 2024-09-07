945fb7b0ea2255ab162b132797e6fa4127ea4c01
MR.IBAY <ibayjimwell@gmail.com>
2024-09-07 17:19:54 +0800

    Creating Mr.Jim

---
+++ mr.jim/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+        <nav className='
+            container
+            mx-auto 
+            mt-10
+        '>
+
+            <div className='
+                text-gray-50
+                text-4xl
+                font-bold
+                font-header
+            '>Mr.Jim</div>
+
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
